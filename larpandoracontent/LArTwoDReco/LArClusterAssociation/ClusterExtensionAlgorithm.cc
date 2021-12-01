44bbbdb452e837a30c7b655186062e5bb16061a5
Andrew Chappell <A.Chappell@warwick.ac.uk>
2021-12-01 09:39:53 +0000

    Add check for cross-volume merges in cluster extensions

--- larpandoracontent/LArTwoDReco/LArClusterAssociation/ClusterExtensionAlgorithm.cc
+++ larpandoracontent/LArTwoDReco/LArClusterAssociation/ClusterExtensionAlgorithm.cc
@@ -8,6 +8,8 @@
 
 #include "Pandora/AlgorithmHeaders.h"
 
+#include "larpandoracontent/LArObjects/LArCaloHit.h"
+
 #include "larpandoracontent/LArTwoDReco/LArClusterAssociation/ClusterExtensionAlgorithm.h"
 
 using namespace pandora;
@@ -19,7 +21,73 @@ void ClusterExtensionAlgorithm::PopulateClusterMergeMap(const ClusterVector &clu
 {
     ClusterAssociationMatrix clusterAssociationMatrix;
     this->FillClusterAssociationMatrix(clusterVector, clusterAssociationMatrix);
+    this->CheckInterTPCVolumeAssociations(clusterAssociationMatrix);
     this->FillClusterMergeMap(clusterAssociationMatrix, clusterMergeMap);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void ClusterExtensionAlgorithm::CheckInterTPCVolumeAssociations(ClusterAssociationMatrix &clusterAssociationMatrix) const
+{
+    ClusterSet unassociatedClusters;
+    for (auto & [ pCluster, clusterAssociationMap ] : clusterAssociationMatrix)
+    {
+        CaloHitList caloHitList;
+        pCluster->GetOrderedCaloHitList().FillCaloHitList(caloHitList);
+        if (caloHitList.empty())
+            continue;
+        // ATTN: Early 2D clustering should preclude input clusters containing mixed volumes, so just check the first hit
+        const LArCaloHit *const pLArCaloHit{dynamic_cast<const LArCaloHit *const>(caloHitList.front())};
+        if (!pLArCaloHit)
+            continue;
+        const unsigned int clusterTpcVolume{pLArCaloHit->GetLArTPCVolumeId()};
+        const unsigned int clusterDaughterVolume{pLArCaloHit->GetDaughterVolumeId()};
+
+        auto assocIter{clusterAssociationMap.begin()};
+        while (assocIter != clusterAssociationMap.end())
+        {
+            const Cluster *pOtherCluster{static_cast<const Cluster *>(assocIter->first)};
+            CaloHitList otherHitList;
+            pOtherCluster->GetOrderedCaloHitList().FillCaloHitList(otherHitList);
+            if (otherHitList.empty())
+                continue;
+            const LArCaloHit *const pLArOtherHit{dynamic_cast<const LArCaloHit *const>(otherHitList.front())};
+            if (!pLArOtherHit)
+                continue;
+            const unsigned int otherTpcVolume{pLArOtherHit->GetLArTPCVolumeId()};
+            const unsigned int otherDaughterVolume{pLArOtherHit->GetDaughterVolumeId()};
+
+            if (clusterTpcVolume == otherTpcVolume && clusterDaughterVolume == otherDaughterVolume)
+            {
+                // Same volume, move on
+                ++assocIter;
+            }
+            else
+            {
+                // Volumes differ, confirm association valid
+                float clusterXmin{0.f}, clusterXmax{0.f}, otherXmin{0.f}, otherXmax{0.f};
+                pCluster->GetClusterSpanX(clusterXmin, clusterXmax);
+                pOtherCluster->GetClusterSpanX(otherXmin, otherXmax);
+                const bool overlap{(clusterXmin >= otherXmin && clusterXmin < otherXmax) ||
+                    (clusterXmax > otherXmin && clusterXmax <= otherXmax) || (clusterXmin <= otherXmin && clusterXmax >= otherXmax)};
+                if (overlap)
+                {
+                    // Drift coordinates overlap across volumes, veto
+                    assocIter = clusterAssociationMap.erase(assocIter);
+                }
+                else
+                {
+                    // No X overlap, move on
+                    ++assocIter;
+                }
+            }
+        }
+        if (clusterAssociationMap.empty())
+            unassociatedClusters.insert(pCluster);
+    }
+
+    for (const Cluster *const pCluster : unassociatedClusters)
+        clusterAssociationMatrix.erase(pCluster);
 }
 
 } // namespace lar_content
