8c7bab59ac8f8a716bcf7af67acd35b6cc1a5ac1
Bruce Howard <howard@fnal.gov>
2023-04-24 10:06:03 -0500

    Also add checks on clusters having hits with different TPC volumes in the ThreeViewTrackFragmentsAlgorithm. This is VERY preliminary at moment, targeted because the algorithm added to check inter-volume clusters was showing some mixing happening during this step.

--- larpandoracontent/LArThreeDReco/LArTrackFragments/ThreeViewTrackFragmentsAlgorithm.cc
+++ larpandoracontent/LArThreeDReco/LArTrackFragments/ThreeViewTrackFragmentsAlgorithm.cc
@@ -10,6 +10,7 @@
 
 #include "larpandoracontent/LArHelpers/LArClusterHelper.h"
 #include "larpandoracontent/LArHelpers/LArGeometryHelper.h"
+#include "larpandoracontent/LArObjects/LArCaloHit.h"
 
 #include "larpandoracontent/LArThreeDReco/LArTrackFragments/ThreeViewTrackFragmentsAlgorithm.h"
 
@@ -168,9 +169,56 @@ void ThreeViewTrackFragmentsAlgorithm::CalculateOverlapResult(const Cluster *con
             : (TPC_VIEW_V == missingHitType) ? this->GetCachedSlidingFitResult(pClusterW) : this->GetCachedSlidingFitResult(pClusterV));
 
     const ClusterList &inputClusterList(this->GetInputClusterList(missingHitType));
+    ClusterList actualInputClusterList;
+
+    // Pare down input cluster list. Testing for now... If Missing view is W then only keep considering the W clusters in the same volume... (how to handle clusters spanning multiple volumes?)
+    if ( TPC_VIEW_W == missingHitType ) {
+      CaloHitList caloHitList1;
+      if ( missingHitType == TPC_VIEW_U ) pClusterV->GetOrderedCaloHitList().FillCaloHitList(caloHitList1);
+      else                                pClusterU->GetOrderedCaloHitList().FillCaloHitList(caloHitList1);
+      if (caloHitList1.empty())
+        throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+      const LArCaloHit *const pLArCaloHit1{dynamic_cast<const LArCaloHit *const>(caloHitList1.front())};
+      if (!pLArCaloHit1)
+        throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+      const unsigned int clusterTpcVolume1{pLArCaloHit1->GetLArTPCVolumeId()};
+      const unsigned int clusterSubVolume1{pLArCaloHit1->GetSubVolumeId()};
+
+      CaloHitList caloHitList2;
+      if ( missingHitType == TPC_VIEW_W ) pClusterV->GetOrderedCaloHitList().FillCaloHitList(caloHitList2);
+      else                                pClusterW->GetOrderedCaloHitList().FillCaloHitList(caloHitList2);
+      if (caloHitList2.empty())
+        throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+      const LArCaloHit *const pLArCaloHit2{dynamic_cast<const LArCaloHit *const>(caloHitList2.front())};
+      if (!pLArCaloHit2)
+        throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+      const unsigned int clusterTpcVolume2{pLArCaloHit2->GetLArTPCVolumeId()};
+      const unsigned int clusterSubVolume2{pLArCaloHit2->GetSubVolumeId()};
+
+      for ( auto const iCluster : inputClusterList ) {
+	CaloHitList caloHitListOther;
+	iCluster->GetOrderedCaloHitList().FillCaloHitList(caloHitListOther);
+	if (caloHitListOther.empty())
+	  continue;
+	const LArCaloHit *const pLArCaloHitOther{dynamic_cast<const LArCaloHit *const>(caloHitListOther.front())};
+	if (!pLArCaloHitOther)
+	  continue;
+	const unsigned int clusterTpcVolumeOther{pLArCaloHitOther->GetLArTPCVolumeId()};
+	const unsigned int clusterSubVolumeOther{pLArCaloHitOther->GetSubVolumeId()};
+
+	if ( clusterTpcVolumeOther==clusterTpcVolume1 && clusterTpcVolumeOther==clusterTpcVolume2 &&
+	     clusterSubVolumeOther==clusterSubVolume1 && clusterSubVolumeOther==clusterSubVolume2 )
+	  actualInputClusterList.push_back( iCluster );
+      }
+    } else {
+      for ( auto const iCluster : inputClusterList ) {
+	actualInputClusterList.push_back( iCluster );
+      }
+    }
+    // --------- (BH)
 
     const Cluster *pBestMatchedCluster(nullptr);
-    const StatusCode statusCode(this->CalculateOverlapResult(fitResult1, fitResult2, inputClusterList, pBestMatchedCluster, newOverlapResult));
+    const StatusCode statusCode(this->CalculateOverlapResult(fitResult1, fitResult2, actualInputClusterList, pBestMatchedCluster, newOverlapResult));
 
     if ((STATUS_CODE_SUCCESS != statusCode) && (STATUS_CODE_NOT_FOUND != statusCode))
         throw StatusCodeException(statusCode);
